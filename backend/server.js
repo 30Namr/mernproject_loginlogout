03833942aa05a6391a80a71ff6177f982f76ca76
Namrata <91418505+30Namr@users.noreply.github.com>
2025-07-20 10:43:09 +0530

    Update server.js
    build

--- backend/server.js
+++ backend/server.js
@@ -19,3 +19,10 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));  
+
+const path = require("path");
+app.use(express.static(path.join(__dirname,'./frontend/build')))
+
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
+})
